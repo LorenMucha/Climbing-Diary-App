ee748f1d405c06730779c3c6f36704a66bbda7d6
lorenmucha <>
2021-11-24 20:07:39 +0100

    add missing setup for sdk in github actions

--- app/src/androidTest/java/com/main/climbingdiary/fragments/SettingsFragmentTest.kt
+++ app/src/androidTest/java/com/main/climbingdiary/fragments/SettingsFragmentTest.kt
@@ -5,6 +5,11 @@ import com.main.climbingdiary.activities.MainActivity
 import com.main.climbingdiary.database.entities.Projekt
 import com.main.climbingdiary.database.entities.Route
 import com.main.climbingdiary.database.entities.RouteRepository
+import com.main.climbingdiary.helper.TestHelper.getRandomProjektList
+import com.main.climbingdiary.helper.TestHelper.getRandomRouteList
+import com.main.climbingdiary.helper.TestSqliteHelper
+import org.junit.After
+import org.junit.Before
 
 internal class SettingsFragmentTest {
     private lateinit var activityScenario: ActivityScenario<MainActivity>
@@ -12,5 +17,19 @@ internal class SettingsFragmentTest {
     private val routeRepo = RouteRepository(Route::class)
     private val projektRepo = RouteRepository(Projekt::class)
 
-    
+    @After
+    fun cleanUp() {
+        TestSqliteHelper.cleanAllTables()
+        activityScenario.close()
+    }
+
+    @Before
+    fun setUp() {
+        activityScenario =
+            ActivityScenario.launch(MainActivity::class.java)
+        getRandomRouteList(50).forEach { routeRepo.insertRoute(it) }
+        getRandomProjektList(10).forEach { projektRepo.insertRoute(it) }
+    }
+
+
 }
