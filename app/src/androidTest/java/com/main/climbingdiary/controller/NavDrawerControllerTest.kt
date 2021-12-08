21f86bd4426f1f92c9b97827b9e33f4e23af5dd8
lorenmucha <>
2021-12-08 16:59:08 +0100

    pipeline fix

--- app/src/androidTest/java/com/main/climbingdiary/controller/NavDrawerControllerTest.kt
+++ app/src/androidTest/java/com/main/climbingdiary/controller/NavDrawerControllerTest.kt
@@ -1,5 +1,24 @@
 package com.main.climbingdiary.controller
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.intent.Intents
+import com.main.climbingdiary.activities.MainActivity
+import org.junit.After
+import org.junit.Before
+
 class NavDrawerControllerTest{
+    private lateinit var activityScenario: ActivityScenario<MainActivity>
+
+    @After
+    fun cleanUp() {
+        activityScenario.close()
+        Intents.release()
+    }
+
+    @Before
+    fun setUp(){
+        activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        Intents.init()
+    }
 
 }
